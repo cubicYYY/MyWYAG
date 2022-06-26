fea3af17357e7047b653432532430bcf234b1066
Your Name <you@example.com>
2022-06-26 23:32:03 +0800

    wyag init and some type hints Need further improvement of type hints and git-like compatibility.

--- libwyag.py
+++ libwyag.py
@@ -6,15 +6,119 @@ import os
 import re
 import sys
 import zlib
+from typing import *
 
 argparser = argparse.ArgumentParser()
 argsubparsers = argparser.add_subparsers(dest="command")
 argsubparsers.required = True
 
-argsp = argsubparsers.add_parser("init", help="Initialize the wyag work directory")
-argsp = argsubparsers.add_parser("add", help="Add files to cache")
+class GitRepository(object):
+    """A basic data model for a git repository"""
 
-def main(argv=sys.argv[1:]):
+    worktree = None # Work directory path
+    gitdir = None # .git directory path
+    conf = None # Configurations
+    
+    def __init__(self, path, force=False):
+        self.worktree = path
+        self.gitdir = os.path.join(path, ".git")
+
+        if not (force or os.path.isdir(self.gitdir)):
+            raise Exception("Not a Git repository %s" % path)
+
+        # Read configuration file in .git/config
+        self.conf = configparser.ConfigParser()
+        cf = repo_file(self, "config")
+
+        if cf and os.path.exists(cf):
+            self.conf.read([cf])
+        elif not force:
+            raise Exception("Configuration file missing")
+
+        if not force:
+            vers = int(self.conf.get("core", "repositoryformatversion"))
+            if vers != 0:
+                raise Exception("Unsupported repositoryformatversion %s" % vers)
+            
+def repo_path(repo: type[GitRepository], *path: str)->Any:
+    """Compute path under repo's gitdir."""
+    return os.path.join(repo.gitdir, *path)
+            
+def repo_file(repo: type[GitRepository], *path: str, mkdir: bool=False)->Any:
+    """Same as repo_path, but create dirname(*path) if absent"""
+    if repo_dir(repo, *path[:-1], mkdir=mkdir):
+        return repo_path(repo, *path)
+
+def repo_dir(repo: type[GitRepository], *path: str, mkdir: bool=False)->Any:
+    """Same as repo_path, but mkdir *path if absent if mkdir."""
+
+    path = repo_path(repo, *path)
+
+    if os.path.exists(path):
+        if (os.path.isdir(path)):
+            return path
+        else:
+            raise Exception("Not a directory %s" % path)
+
+    if mkdir:
+        os.makedirs(path)
+        return path
+    else:
+        return None
+       
+def repo_default_config():
+    ret = configparser.ConfigParser()
+
+    ret.add_section("core")
+    ret.set("core", "repositoryformatversion", "0")
+    ret.set("core", "filemode", "false")
+    ret.set("core", "bare", "false")
+
+    return ret
+
+argsp = argsubparsers.add_parser("init", help="Initialize a new, empty repository.")
+argsp.add_argument("path",
+                   metavar="directory",
+                   nargs="?",
+                   default=".",
+                   help="Where to create the repository.")
+
+def repo_create(path):
+    """Create a new repository at path."""
+
+    repo = GitRepository(path, True)
+
+    # First, we make sure the path either doesn't exist or is an
+    # empty dir.
+
+    if os.path.exists(repo.worktree):
+        if not os.path.isdir(repo.worktree):
+            raise Exception ("%s is not a directory!" % path)
+        if os.listdir(repo.worktree):
+            raise Exception("%s is not empty!" % path)
+    else:
+        os.makedirs(repo.worktree)
+
+    assert(repo_dir(repo, "branches", mkdir=True))
+    assert(repo_dir(repo, "objects", mkdir=True))
+    assert(repo_dir(repo, "refs", "tags", mkdir=True))
+    assert(repo_dir(repo, "refs", "heads", mkdir=True))
+
+    # .git/description
+    with open(repo_file(repo, "description"), "w") as f:
+        f.write("Unnamed repository; edit this file 'description' to name the repository.\n")
+
+    # .git/HEAD
+    with open(repo_file(repo, "HEAD"), "w") as f:
+        f.write("ref: refs/heads/master\n")
+
+    with open(repo_file(repo, "config"), "w") as f:
+        config = repo_default_config()
+        config.write(f)
+
+    return repo
+            
+def main(argv: list[str] =sys.argv[1:])->None:
     args = argparser.parse_args(argv)
     
     if   args.command == "add"         : cmd_add(args)
@@ -32,8 +136,8 @@ def main(argv=sys.argv[1:]):
     # elif args.command == "show-ref"    : cmd_show_ref(args)
     # elif args.command == "tag"         : cmd_tag(args)
     
-def cmd_init(args):
-    print("init")
+def cmd_init(args)->None:
+    repo_create(args.path)
 
-def cmd_add(args):
-    print("add")
+def cmd_add(args)->None:
+    pass
